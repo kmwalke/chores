99ce93472e72bfdb8d1359964ef0198408625da9
Kent Slaymaker <kent@1bg.com>
2020-12-06 13:00:35 -0700

    [7_access_control] add permission actions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,4 +3,11 @@ class Permission < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
+  DELETE = 'delete'.freeze
+  CREATE = 'create'.freeze
+  UPDATE = 'update'.freeze
+  READ   = 'read'.freeze
+
+  ACTIONS = [DELETE, CREATE, UPDATE, READ].freeze
 end
