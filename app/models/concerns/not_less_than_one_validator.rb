a7ea72adef6aaedc1421e032154c6c032f6fdb49
Kent Slaymaker <kent@1bg.com>
2020-12-06 10:35:52 -0700

    remove comment

--- app/models/concerns/not_less_than_one_validator.rb
+++ app/models/concerns/not_less_than_one_validator.rb
@@ -1,6 +1,5 @@
 class NotLessThanOneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    # binding.pry
     record.errors[attribute] << (options[:message] || 'cannot be less than one') unless value >= 1
   end
 end
