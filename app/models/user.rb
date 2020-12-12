9daf435944608101dcc48c18749ad634a4d8fed1
Kent Slaymaker <kent@1bg.com>
2020-12-12 12:16:57 -0700

    [12_tasks] task model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   belongs_to :role
+  has_many :tasks
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, email: true
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:task) { FactoryBot.create(:task) }
+  it 'should require a name' do
+    expect(Task.create(name: '').errors).to have_key(:name)
+  end
+
+  it 'should require a user' do
+    expect(Task.create(user_id: nil).errors).to have_key(:user_id)
+  end
+
+  it 'should require a frequency' do
+    expect(Task.create(frequency: '').errors).to have_key(:frequency)
+  end
+
+  it 'should require a positive frequency' do
+    expect(Task.create(frequency: 0).errors).to have_key(:frequency)
+  end
+
+  it 'should require a size' do
+    expect(Task.create(size: '').errors).to have_key(:size)
+  end
+
+  it 'should require a positive size' do
+    expect(Task.create(size: 0).errors).to have_key(:size)
+  end
+
+  it 'should generate an xp value' do
+    expect(FactoryBot.create(:task).xp > 0).to eq(true)
+  end
+
+  it 'should update the xp value' do
+    old_xp = task.xp
+    task.update_attribute(:size, task.size + 1)
+    expect(task.xp > old_xp).to eq(true)
+  end
+
+  it 'should not allow xp to be set' do
+    expect(Task.new.respond_to?('xp=')).to eq(false)
+  end
+
+  it 'should be completed' do
+    # create a task.complete method that creates a completion for the task
+  end
 end
