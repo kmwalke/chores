d55ff564e03efab8cc42a261aef9e1a5fa495cb5
Kent Slaymaker <kent@1bg.com>
2020-12-16 11:54:02 -0700

    [25_daily_tasks] dont start login task on circle

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user&.authenticate(params[:password])
-      InstantiateTasksWorker.perform_async(user.id)
+      InstantiateTasksWorker.perform_async(user.id) unless ENV['CIRCLECI']
       session[:user_id]          = user.id
       destination                = session[:orig_destination] || root_path
       session[:orig_destination] = nil
