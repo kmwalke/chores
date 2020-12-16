289dc123fac5e9277f55a9e3ed2adc044e49f4a6
Kent Slaymaker <kent@1bg.com>
2020-12-16 13:01:24 -0700

    [13_rewards] reward model spec

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,2 +1,4 @@
 class Reward < ApplicationRecord
+  validates :name, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Reward, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should require a name' do
+    expect(Reward.create(name: '').errors).to have_key(:name)
+  end
+
+  it 'should require a user' do
+    expect(Reward.create(user_id: nil).errors).to have_key(:user_id)
+  end
 end
