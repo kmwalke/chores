a62e9dde16c9e976e47f6887715cdab6c6289e74
Kent Slaymaker <kent@1bg.com>
2020-12-17 16:17:11 -0700

    [21_main_page] sort tasks and rewards on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @user           = current_user
-    @task_list      = current_user&.task_list
-    @earned_rewards = current_user&.earned_rewards
+    @task_list      = current_user&.task_list&.sort
+    @earned_rewards = current_user&.earned_rewards&.sort
   end
 end
