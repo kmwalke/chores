4a98a79e2b320e46055fc01745a13ccf6ed90eab
Kent Slaymaker <kent@1bg.com>
2020-12-16 18:27:53 -0700

    [21_main_page] update home_spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature 'Home', type: :feature do
   scenario 'displays the homepage' do
     visit root_path
 
-    expect(page).to have_content('This is the index')
+    expect(page).to have_content('Chores App')
   end
 end
