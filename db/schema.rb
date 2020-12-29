eec90df909d5ca3c7ebf5236674c8d757cdc4357
Kent Slaymaker <kent@1bg.com>
2020-12-29 09:52:22 -0700

    [49_timezones] add timezone to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_214623) do
+ActiveRecord::Schema.define(version: 2020_12_29_163841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_12_16_214623) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "next_reward_id"
+    t.string "time_zone", default: "UTC", null: false
   end
 
 end
