90f78bbcf72eb7faebcb479265adcf5dca0885d1
Kent Slaymaker <kent@1bg.com>
2021-01-01 11:40:45 -0700

    [20_links] only show allowed links

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,9 @@ class Role < ApplicationRecord
   has_many :user
 
   validates :name, presence: true, uniqueness: true
+
+  def has_feature(name)
+    feature = Feature.find_by(name: name.downcase)
+    permissions.select{ |p| p.feature_id == feature.id}.any? unless feature.nil?
+  end
 end
