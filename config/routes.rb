4c4ddb56d01d0fdf6e0bf09603f3da49057d5f6d
Kent <kentslaymaker@gmail.com>
2025-05-13 13:02:15 -0600

    UI fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
 
   root to: 'home#index'
-  patch 'complete/:id', to: 'home#complete', as: 'complete_task_instance'
+  get 'complete/:id', to: 'home#complete', as: 'complete_task_instance'
 
   get 'current_user/edit', as: 'edit_current_user'
   patch 'current_user/update', as: 'update_current_user'
